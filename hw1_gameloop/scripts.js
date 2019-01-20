e882e36df7c7c15caccc9155a2016f76c59dd78d
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-01-19 22:45:48 -0700

    Adjusted index.html to only have a <td> in box_1. Also started on functions in scripts.js

---
+++ hw1_gameloop/scripts.js
@@ -0,0 +1,20 @@
+function gameLoop() {
+    let gameTime = performance.now();
+
+    console.log(gameTime);
+    let node = document.getElementById('box_1');
+    node.innerHTML += "Current Time: " + gameTime + "\n";
+    //let y = node.scrollTop;
+}
+
+function update(elapsed_time) {
+
+}
+
+function render() {
+
+}
+
+function newEvent(name, interval, report_times) {
+
+}
