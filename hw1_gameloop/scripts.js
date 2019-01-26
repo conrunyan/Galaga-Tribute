ac745575739f4c02e95c9c4ea5eca1ae3e4e8740
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-01-25 22:47:52 -0700

    Finally got elapsed time working correctly

--- hw1_gameloop/scripts.js
+++ hw1_gameloop/scripts.js
@@ -1,51 +1,69 @@
 let events = [];
 let startTime = performance.now();
-let ERROR_MARGIN = 10; 
+let ERROR_MARGIN = 20; 
+let topOfLoop = true;
+let prevBrowserTime = 0;
 gameLoop();
 
 function gameLoop(browserTime) {
     // DONE: Get elapsed time
-    let elapsedTime = browserTime - startTime;
-    // TODO: Add update funciton
+    let elapsedTime = 0
+    if (!topOfLoop) {
+        elapsedTime = browserTime - prevBrowserTime;
+        prevBrowserTime = browserTime;
+    }
+    // DONE: Add update funciton
     update(elapsedTime);
-    // TODO: Add render function
-    // let node = document.getElementById('box_1');
-    // node.innerHTML += "Current Time: " + gameTime + "\n";
-    //console.log(myTime);
+    // DONE: Add render function
+    render()
     //let y = node.scrollTop;
+    topOfLoop = false;
     requestAnimationFrame(gameLoop);
 }
 
 function update(elapsedTime) {
     // DONE: Add logic to calculate if an object should be rendered. Based on interval
     for (let i = 0; i < events.length; i++) {
-        //console.log('MATH FLOOR: ' + Math.floor(elapsedTime) + ' => ' + parseInt(events[i].interval))
+        console.log('TOP OF LOOP: ' + Math.floor(elapsedTime) + ' => ' + parseInt(events[i].interval))
         // if an event has 0 times left to display, mark for deletion
         if (parseInt(events[i].times) === 0) {
             events[i].delete = true;
         }
         // if the elapsed time is a multiple (within a margin of error) of the interval, flag event to render
-        else if (Math.floor(elapsedTime) % parseInt(events[i].interval) <= ERROR_MARGIN) {
-            console.log('event #: ' + i + events[i]);
+        else if (events[i].timeSinceLastRender >= events[i].interval) {
+            console.log('BEFORE: event #: ' + i + events[i].timeSinceLastRender);
             events[i].render = true;
             events[i].times--;   
-            console.log('event times: ' + events[i].times) 
+            // carry over extra elapsed time
+            events[i].timeSinceLastRender = (events[i].timeSinceLastRender - events[i].interval);
+            console.log('event times: ' + events[i].times); 
+            console.log('AFTER : event #: ' + i + events[i].timeSinceLastRender);
         }
         else
         {
+            // capture elapsed time so far and make sure event isn't rendered
+            events[i].timeSinceLastRender += Math.floor(elapsedTime);
+            console.log('NO RENDER: event #: ' + events[i].timeSinceLastRender);
             events[i].render = false; 
         }
         //console.log('event #: ' + i + events[i]);
     }
-    // TODO: Add step to delete object if # of times is 0
+    // DONE: Add step to delete object if # of times is 0
     // filter out any events flagged to be deleted
-    events = events.filter(tmp_event => tmp_event.delete === false)
-    // Keep objects ordered by time added.
+    events = events.filter(tmpEvent => tmpEvent.delete === false);
+    // TODO: Keep objects ordered by time added.
 }
 
 function render() {
     // TODO: For each event that needs to be rendered, do so.
     let node = document.getElementById('box_1')
+    // For each event flagged to be rendered, print it to box_1
+    events.forEach(function(tmpEvent) {
+        if (tmpEvent.render === true) {
+            node.innerHTML += '<p>Event: ' + tmpEvent.name + '(' + tmpEvent.times + ' remaining)' + '</p>'
+        }
+    });
+    
 }
 
 function addNewEvent() {
@@ -55,11 +73,11 @@ function addNewEvent() {
     let times = document.getElementById('id-times');
 
     name = name.value;
-    interval = interval.value;
-    times = times.value;
+    interval = parseInt(interval.value);
+    times = parseInt(times.value);
     // make sure all fields are populated, otherwise ignore the command
     if (name !== '' && interval !== '' && times !== '') {
-        let newEvent = {name:name , interval:interval, times:times, render:true, delete:false};
+        let newEvent = {name:name , interval:interval, times:times, render:true, delete:false, timeSinceLastRender:0};
         events.push(newEvent);
         console.log(events);
     } 
