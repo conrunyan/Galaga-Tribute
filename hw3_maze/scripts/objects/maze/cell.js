516489fd339b4dd2f11b2fefca6a593c9487d711
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-02-10 21:07:44 -0700

    Base of Cell class built

--- hw3_maze/scripts/objects/maze/cell.js
+++ hw3_maze/scripts/objects/maze/cell.js
@@ -3,14 +3,36 @@
 // Creates a maze Cell object, with functions for managing state.
 // A single Cell can border up 4 other cells, connected by one wall each.
 // spec = {
-//  TBD
+//  xCoord: cell's x coordinate,
+//  yCoord: cell's y coordinate,
+//  walls: {
+//            topWall: ,
+//            bottomWall: ,
+//            leftWall: ,
+//            rightWall: ,
+//          },
+//  cellType: type of cell (start, end, normal),
+//  visited: ,
 // }
 //
 // --------------------------------------------------------------
 MazeGame.objects.maze.Cell = function (spec) {
     'use strict';
 
+    function setVisited (visited) {
+        spec.visited = visited;
+    }
+
     let api = {
+        get xCoord() { return spec.xCoord },
+        get yCoord() { return spec.yCoord },
+        get topWall() { return spec.walls.topWall },
+        get bottomWall() { return spec.walls.bottomWall },
+        get leftWall() { return spec.walls.leftWall },
+        get rightWall() { return spec.walls.rightWall },
+        get cellType() { return spec.cellType },
+        get visited() { return visited },
+        setVisited: setVisited,
     };
 
     return api;
