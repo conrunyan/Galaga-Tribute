129e4c547a26f727d2cd447ccfa89412c0a67005
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-02-10 21:19:42 -0700

    Base for Maze class. Starting on maze generation algorithm

--- hw3_maze/scripts/objects/maze/maze.js
+++ hw3_maze/scripts/objects/maze/maze.js
@@ -4,12 +4,25 @@
 // One Maze object will exist at a time. This can be thougth of as
 // the game board.
 // spec = {
-//  TBD
+//  size: {xSize , ySize: },
 // }
 //
 // --------------------------------------------------------------
 MazeGame.objects.maze.Maze = function (spec) {
     'use strict';
+
+    let mazeBoard = [];
+    let startXY = {x: 0, y: 0}; // default locations
+    let endXY = {x: spec.xSize - 1, y: spec.ySize - 1}; // default locations
+    let shortestPath = [];
+    let breadCrumbs = [];
+    let 
+
+    // Function uses Randomized Prim's algorithm to generate a maze
+    function generateMaze() {
+        // 1 - Gnerate a grid of walls
+
+    }
 
     let api = {
     };
