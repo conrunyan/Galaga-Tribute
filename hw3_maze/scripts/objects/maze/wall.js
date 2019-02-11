6bb89c99eba1f96ea4a6ee699c7224f6dcfcbf87
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-02-10 20:57:07 -0700

    Base of wall class built

--- hw3_maze/scripts/objects/maze/wall.js
+++ hw3_maze/scripts/objects/maze/wall.js
@@ -4,14 +4,24 @@
 // One Wall object can link two cells together, or exist as the end
 // of the board.
 // spec = {
-//  TBD
+//  nodeA: mazeCellA,
+//  nodeB: mazeCellB,
 // }
 //
 // --------------------------------------------------------------
 MazeGame.objects.maze.Wall = function (spec) {
     'use strict';
 
+    let display = true;
+
+    function setDisplay (displayVal) {
+        display = displayVal;
+    }
+
     let api = {
+        get nodeA() { return spec.nodeA },
+        get nodeB() { return spec.nodeB },
+        setDisplay: setDisplay,
     };
 
     return api;
