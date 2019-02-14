c8be0536db2d58c148b5e8bcdf5f1ffabb697d80
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-02-14 00:40:22 -0700

    not the render step...

--- hw3_maze/scripts/render/core.js
+++ hw3_maze/scripts/render/core.js
@@ -51,23 +51,25 @@ MazeGame.graphics = (function () {
             spec.size,
             spec.size);
         if (spec.edges.topWall === undefined) {
-            context.moveTo(spec.rowIdx * spec.size, spec.colIdx * spec.size);
-            context.lineTo((spec.rowIdx + 1) * spec.size, spec.colIdx * spec.size);
+            context.moveTo(spec.xCoord, spec.yCoord);
+            context.lineTo(spec.xCoord + spec.size, spec.yCoord);
         }
 
         if (spec.edges.bottomWall === undefined) {
-            context.moveTo(spec.rowIdx * spec.size, (spec.colIdx + 1) * spec.size);
-            context.lineTo((spec.rowIdx + 1) * spec.size, (spec.colIdx + 1) * spec.size);
+            // context.moveTo(spec.rowIdx * spec.size, (spec.colIdx + 1) * spec.size);
+            // context.lineTo((spec.rowIdx + 1) * spec.size, (spec.colIdx + 1) * spec.size);
+            context.moveTo(spec.xCoord, spec.yCoord + spec.size);
+            context.lineTo(spec.xCoord + spec.size, spec.yCoord + spec.size);
         }
 
         if (spec.edges.rightWall === undefined) {
-            context.moveTo((spec.rowIdx + 1) * spec.size, spec.colIdx * spec.size);
-            context.lineTo((spec.rowIdx + 1) * spec.size, (spec.colIdx + 1) * spec.size);
+            context.moveTo(spec.xCoord + spec.size, spec.yCoord);
+            context.lineTo(spec.xCoord + spec.size, spec.yCoord - spec.size);
         }
 
         if (spec.edges.leftWall === undefined) {
-            context.moveTo(spec.rowIdx * spec.size, spec.colIdx * spec.size);
-            context.lineTo(spec.rowIdx * spec.size, (spec.colIdx + 1) * spec.size);
+            context.moveTo(spec.xCoord, spec.yCoord - spec.size);
+            context.lineTo(spec.xCoord, spec.yCoord + spec.size);
         }
         context.stroke();
         context.restore();
