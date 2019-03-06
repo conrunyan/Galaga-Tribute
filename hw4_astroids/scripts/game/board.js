21dfab40ffc9570ba8c519161edc61d45a3a906d
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-03-06 16:29:41 -0700

    Added board and assets to git

---
+++ hw4_astroids/scripts/game/board.js
@@ -0,0 +1,47 @@
+// --------------------------------------------------------------
+//
+// Creates a Player object, with functions for managing state.
+// One player object will exist at a time. This object can manuever
+// around the game board and interact with the maze (exit, run into walls, etc.)
+// spec = {
+//  gamePieces: {
+//      player: {playerObj},
+//      asteroids: [{asteroidObj}, ...],
+//      ufos: [{ufoObj}, ....]
+//  }
+//  backgroundImage: image
+// }
+// --------------------------------------------------------------
+Asteroids.game.Board = (function (spec) {
+    'use strict';
+
+    // load image
+    let backgroundImg = new Image();
+    backgroundImg.isReady = false;
+    backgroundImg.src = spec.imageSrc;
+    backgroundImg.onload = function () {
+        console.log('loaded image...');
+        this.isReady = true;
+    };
+
+    function addAsteroid(newAstrd) {
+        spec.gamePieces.asteroids.push(newAstrd);
+    }
+
+    function addUFO(newUFO) {
+        spec.gamePieces.ufos.push(newUFO);
+    }
+
+    function updatePieces() {
+        // update player
+        spec.gamePieces.player.update();
+    }
+
+    let api = {
+        get image() { return backgroundImg },
+        addAsteroid: addAsteroid,
+        addUFO: addUFO,
+    }
+
+    return api;
+}(Asteroids.objects.player));
--- notes/js_examples/first_js_example.js
+++
@@ -1,2 +0,0 @@
-let message = 'THIS IS A MESSAGE';
-console.log(message); // sends message to log in the console inside the browser
