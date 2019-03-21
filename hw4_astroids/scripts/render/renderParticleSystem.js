72dcd209889769f716e8e4ea277cb6e91089e223
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-03-21 17:46:43 -0600

    Working on adding particle system

---
+++ hw4_astroids/scripts/render/renderParticleSystem.js
@@ -0,0 +1,24 @@
+Asteroids.render.ParticleSystem = function (system, graphics, imageSrc) {
+    let image = new Image();
+    let isReady = false;
+
+    image.onload = () => {
+        isReady = true;
+    };
+    image.src = imageSrc;
+
+    function render() {
+        if (isReady) {
+            Object.getOwnPropertyNames(system.particles).forEach(function (value) {
+                let particle = system.particles[value];
+                graphics.drawTexture(image, particle.center, particle.rotation, particle.size);
+            });
+        }
+    }
+
+    let api = {
+        render: render
+    };
+
+    return api;
+};
