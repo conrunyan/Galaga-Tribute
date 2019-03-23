c343197f9056f46ea5aeacfd46d66dadae447480
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-03-23 13:40:32 -0600

    Added assets and adjustments to render

---
+++ hw4_astroids/scripts/render/renderGameStatus.js
@@ -0,0 +1,20 @@
+Asteroids.render.Status = (function (graphics, renderer) {
+    'use strict';
+
+    //drawTexture(image, center, rotation, size)
+    function renderStats(player, board) {
+        // render player stats
+        let playerScore = `Score: ${player.score}`;
+        let playerLives = `Lives: ${player.lives}`;
+        graphics.drawText(playerScore, {x: 10, y: 30}, 'white', 12);
+        graphics.drawText(playerLives, { x: 10, y: 50 }, 'white', 12);
+
+    }
+
+    let api = {
+        renderStats: renderStats,
+    };
+
+    return api;
+
+}(Asteroids.graphics, Asteroids.render));
