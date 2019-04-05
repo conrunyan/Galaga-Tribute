0138e46960c9fc6ca1dc9542c58e5abf4a312ac2
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-04-04 22:34:30 -0600

    Stats display correct colors

--- exam_game/template_code/scripts/render/renderGameStatus.js
+++ exam_game/template_code/scripts/render/renderGameStatus.js
@@ -11,9 +11,9 @@ Galaga.render.Status = (function (graphics, renderer) {
         let playerAngle = `Angle: ${player.displayRot}`;
         let angleColor = player.displayAngleColor;
         // let playerHyperspace = `Hyper-Space: N/A`;
-        graphics.drawText(playerScore, {x: 10, y: 30}, 'white', 12);
-        graphics.drawText(playerSpeed, { x: 10, y: 50 }, 'white', 12);
-        graphics.drawText(playerAngle, { x: 10, y: 70 }, 'white', 12);
+        graphics.drawText(playerScore, { x: 10, y: 30 }, scoreColor, 12);
+        graphics.drawText(playerSpeed, { x: 10, y: 50 }, speedColor, 12);
+        graphics.drawText(playerAngle, { x: 10, y: 70 }, angleColor, 12);
         // graphics.drawText(playerHyperspace, { x: 10, y: 90 }, 'white', 12);
 
     }
