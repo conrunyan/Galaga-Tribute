4e07f0adbdec57945ac694d284256cfadd55bce8
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-04-06 17:27:00 -0600

    finishied base of grid setup

--- fp_galaga/scripts/objects/hazards/alienGrid.js
+++ fp_galaga/scripts/objects/hazards/alienGrid.js
@@ -12,19 +12,12 @@
 Galaga.objects.ufo.AlienGrid = function (spec) {
     'use strict';
 
-    // load image
-    let image = new Image();
-    image.isReady = false;
-    image.src = spec.imageSrc;
-    image.onload = function () {
-        // console.log('loaded image...');
-        this.isReady = true;
-    };
-
     let movementSpeed = 0.0002;
     let timeForEachMovement = 1000;
     let grid = [];
     let gridMargin = 20; // in pixels
+    let xOffset = 0;
+    let yOffset = 0;
 
     // moves grid containing aliens
     function moveGrid(elapsedTime) {
@@ -46,6 +39,14 @@ Galaga.objects.ufo.AlienGrid = function (spec) {
     //}
     function GridSlot(gridSpecs) {
 
+        let image = new Image();
+        image.isReady = false;
+        image.src = spec.imageSrc;
+        image.onload = function () {
+            // console.log('loaded image...');
+            this.isReady = true;
+        };
+
         function setCoords(coords) {
             gridSpecs.coords = coords;
         }
@@ -60,6 +61,7 @@ Galaga.objects.ufo.AlienGrid = function (spec) {
         }
 
         let api = {
+            get image() { return image },
             get coords() { return gridSpecs.coords },
             get available() { return _isAvailable() },
             get contains() { return gridSpecs.contains },
@@ -67,28 +69,33 @@ Galaga.objects.ufo.AlienGrid = function (spec) {
         }
     }
 
+    function _getNextSlotCoords(newXOffset, newXOffset) {
+        return { x: spec.coords.x + newXOffset, y: spec.coords.y + newXOffset }
+    }
+
     // set up empty grid of places for aliens
     function initGrid() {
         for (let row = 0; row < spec.gridHeight; row++) {
             let newRow = []
             for (let col = 0; col < spec.gridWidth; col++) {
+                let newCoords = _getNextSlotCoords(xOffset, yOffset)
+                let newSlot = GridSlot({
+                    coords: newCoords,
 
+                });
+                newRow.push(newSlot);
+                // update xOffset
+                xOffset += gridMargin;
             }
+            grid.push(newRow);
+            // update yOffset
+            yOffset += gridMargin;
         }
     }
 
     let api = {
-        get image() { return image },
         get coords() { return spec.coords },
-        get size() { return spec.size },
-        get radius() { return spec.size / 2 },
-        get rotation() { return spec.rotation },
-        get projectiles() { return projectiles },
-        get didCollide() { return didCollide },
-        get ufoType() { return spec.ufoType },
-        get center() { return { x: spec.coords.x + (spec.size / 2), y: spec.coords.y + (spec.size / 2), } },
-        get shouldExplode() { return lifeTime <= 0 },
-        get points() { return points },
+        get grid() { return grid },
         moveGrid: moveGrid,
     };
 
